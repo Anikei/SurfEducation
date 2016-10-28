886a24224de69dc35b39c27d9038757fa9d94828
= <=>
2016-10-28 20:17:32 +0300

    add activity methods:

--- app/src/main/java/com/surf/education/surfeducation/MainActivity.java
+++ app/src/main/java/com/surf/education/surfeducation/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+
+//startandroid.ru/ru/uroki/vse-uroki-spiskom/
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,7 +19,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //также можно использовать, но редко onRestoreInstanceState(savedInstanceState);
         Log.d("TAG", "onCreate");
+        if (savedInstanceState != null) {
+            String label = savedInstanceState.getString("label");
+            Log.d("TAG", label);
+        }
     }
 
     @Override
@@ -38,22 +45,40 @@ public class MainActivity extends AppCompatActivity {
         Log.d("TAG", "onResume");
     }
 
+    //Все данные сохраняются в этот момент (т.к. переходи на другую активити)
     @Override
     protected void onPause() {
         super.onPause();
         Log.d("TAG", "onPause");
+        //отписка от листенеров, стартовавших в onResume()
     }
 
+    //при переходе на другую активити для текущей отработает только после отработки
+    //на новой активити onCreate(), onStart(), onResume()
     @Override
     protected void onStop() {
         super.onStop();
         Log.d("TAG", "onStop");
+        //убираем то, что инициализировано в онСтарт
     }
 
+    //гарантированно вызывается методом finish()
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.d("TAG", "onDestroy");
     }
 
+    /**
+     * активити удаляется и пересоздается при повороте устройства
+     * сюда вcё надо сохранить
+     */
+    //если виджет имеет id, он сохраняет состояния автоматически
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("label", "Restored!!!");
+        //всё это вернется в onCreate()
+        //не использовать для сохранения данных, отличных от
+    }
 }
