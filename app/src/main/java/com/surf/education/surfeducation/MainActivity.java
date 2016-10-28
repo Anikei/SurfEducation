5872ead71b9ef091313fa7f28f6a8dfc89a7bf3e
Жигуненко Никита Александорович <zhigunenko_na@AMM.VSU.RU>
2016-10-28 19:09:10 +0300

    initial

---
+++ app/src/main/java/com/surf/education/surfeducation/MainActivity.java
@@ -0,0 +1,15 @@
+package com.surf.education.surfeducation;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.main_activity);
+
+    }
+}
